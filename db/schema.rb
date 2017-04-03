1a76f61c78ddf055efc25ebf7f732296999fe829
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 16:18:12 +0900

    set confirmable to master model

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20170402080905) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_masters_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_masters_on_reset_password_token", unique: true, using: :btree
   end

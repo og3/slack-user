9b0b8d7a3b98cef227f73acc6cd4c8048791166b
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 16:12:37 +0900

    set routes
    rootと各種コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :masters
+  devise_for :masters, controllers: {
+    registrations: 'masters/registrations', 
+    confirmations: 'masters/confirmations',
+    sessions: 'masters/sessions',
+    passwords: 'masters/passwords',
+    invitations: 'masters/invitations'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'masters#new'
 end

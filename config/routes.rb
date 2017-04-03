0c7cb1a96b697c095832741bf71e55b323be71f8
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 20:37:43 +0900

    add resources team

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     invitations: 'masters/invitations'
     }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'masters#new'
+  root 'masters#new'
+  resources :teams
 end

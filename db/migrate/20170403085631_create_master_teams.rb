82de4cfff2a8c033b93caa0533ea1ace6b6d32fb
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 17:58:55 +0900

    create master_team table

--- db/migrate/20170403085631_create_master_teams.rb
+++ db/migrate/20170403085631_create_master_teams.rb
@@ -1,7 +1,8 @@
 class CreateMasterTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :master_teams do |t|
-
+      t.references :master, foreign_key: true, null: false
+      t.references :team, foreign_key: true
       t.timestamps
     end
   end

7ff76f764a9c0a844af4bba7bc813417cdc6a8bb
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 16:31:08 +0900

    set confirm mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,18 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 } 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Settings.gmail["user_name"],
+    :password => Settings.gmail["password"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 

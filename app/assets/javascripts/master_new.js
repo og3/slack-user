32f35579912cc09971a905cbe1728b50924edd53
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 16:12:09 +0900

    create master_new view

---
+++ app/assets/javascripts/master_new.js
@@ -0,0 +1,24 @@
+$(function() {
+
+// 未入力に対するアラートと画面の切り替え
+  $('.master_email__headline__button').click('submit', function() {
+    var email = $('.master_email__headline__email').val();
+    if (email.length == 0) {
+      alert("emailを入力してください");
+    } else {
+      $('.email').fadeOut(0);
+      $('.master_header_password').css('background-color', 'white');
+      $('.password').fadeIn(900);
+    }
+  });
+
+// enterでsubmitが押されないようenterを封印
+  $("input").on("keydown", function(e) {
+      if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
+          return false;
+      } else {
+          return true;
+      }
+  });
+
+});

0a552f29d8694bdba7a24ae9049dbaf212e31716
noriyuki togashi <n-togashi@xxxx.local>
2017-04-03 18:00:15 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403085036) do
+ActiveRecord::Schema.define(version: 20170403085631) do
+
+  create_table "master_teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "master_id",  null: false
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["master_id"], name: "index_master_teams_on_master_id", using: :btree
+    t.index ["team_id"], name: "index_master_teams_on_team_id", using: :btree
+  end
 
   create_table "masters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at",                          null: false
@@ -41,4 +50,6 @@ ActiveRecord::Schema.define(version: 20170403085036) do
     t.datetime "updated_at",               null: false
   end
 
+  add_foreign_key "master_teams", "masters"
+  add_foreign_key "master_teams", "teams"
 end

b10b1edc1dc6c66a16266732d6b3e5f6e35c2bce
noriyuki togashi <n-togashi@xxxx.local>
2017-04-04 09:03:23 +0900

    create new create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def new
+    # userインスタンスを作成
+    @user = User.new
+  end
+
+  def create
+    # userインスタンスに入力された値を代入
+    @user = User.new(user_params)
+    if @user.save
+      # message機能追加後にリダイレクト先を変更する
+      redirect_to root_path
+    else
+      flash.now[:alert] = "すべての項目を入力してください"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+  # 入力された値のうち、指定したカラムに入る値のみ保存する処理。加えて、user作成時にmaster_idとteam_idを保存する。
+    params.require(:user).permit(:first_name, :last_name, :user_name{ master_ids:[], team_ids:[] })
+  end
+
 end

3321cb7d2cc57b87dd2e20715ece12d29fa2cf7f
noriyuki togashi <n-togashi@xxxx.local>
2017-04-04 10:07:17 +0900

    add redirect_to master index path

--- app/controllers/masters/sessions_controller.rb
+++ app/controllers/masters/sessions_controller.rb
@@ -7,9 +7,13 @@ class Masters::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    redirect_to masters_path
+  end
 
   # DELETE /resource/sign_out
   # def destroy

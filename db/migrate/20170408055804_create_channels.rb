8ddd1bd166350f9695b0205b21291219b590b7dc
noriyuki togashi <n-togashi@xxxx.local>
2017-04-08 15:02:27 +0900

    rake db:migrate

--- db/migrate/20170408055804_create_channels.rb
+++ db/migrate/20170408055804_create_channels.rb
@@ -1,7 +1,7 @@
 class CreateChannels < ActiveRecord::Migration[5.0]
   def change
     create_table :channels do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403233438) do
+ActiveRecord::Schema.define(version: 20170408055804) do
+
+  create_table "channels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "master_teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "master_id",  null: false

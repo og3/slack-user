9522f63b6653b4311778e6b941559ba20661b8b6
noriyuki togashi <n-togashi@xxxx.local>
2017-04-08 15:32:57 +0900

    add column channel_id user_id

--- db/migrate/20170408063007_create_channel_users.rb
+++ db/migrate/20170408063007_create_channel_users.rb
@@ -1,7 +1,8 @@
 class CreateChannelUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :channel_users do |t|
-
+      t.references :channel, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end

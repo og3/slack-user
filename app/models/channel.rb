e945e9be39a59dc928ffa2b50e2e77b983be2aa4
noriyuki togashi <n-togashi@xxxx.local>
2017-04-10 17:28:03 +0900

    add association channel and team

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,5 @@
 class Channel < ApplicationRecord
   has_many :users, through: :channel_users
   has_many :channel_users
+  belongs_to :team
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   has_many :masters, through: :master_teams
   has_many :master_teams
   has_many :users
+  has_many :channels
 end

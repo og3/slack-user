53c56760409741fba4dc5de45d7b58e32c1d79f7
noriyuki togashi <n-togashi@xxxx.local>
2017-04-11 15:45:14 +0900

    create index action for ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    # users_tableから曖昧検索をして変数usersに入れる やり方は、where("カラム名 like '%検索テキスト%'")
+    @users = User.where("user_name like '%#{params[:user_name]}%'")
+    # debugger
+    # json形式で値を返す
+    render json: @users
+  end
+
   def new
     # userインスタンスを作成
     @user = User.new

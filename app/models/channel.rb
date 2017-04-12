fa813400e8e4e36e0b5518478555f81a6390d2a0
noriyuki togashi <n-togashi@xxxx.local>
2017-04-12 17:14:08 +0900

    add dependent: :delete_all

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,7 @@
 class Channel < ApplicationRecord
   validates :name, presence: true
   has_many :users, through: :channel_users
-  has_many :channel_users
+  # channelが消えると中間テーブルのレコードも消える。
+  has_many :channel_users, dependent: :delete_all
   belongs_to :team
 end

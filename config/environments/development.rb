8284a5fdf53933151bc835a5d5df6eff3f3bc4c3
noriyuki togashi <n-togashi@xxxx.local>
2017-04-14 14:35:50 +0900

    add mail setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 } 
-  
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "slack-user_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = {  host: '52.199.147.57', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Settings.gmail["user_name"],
+    :password => Settings.gmail["password"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

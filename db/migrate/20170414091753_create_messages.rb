f52a39ea3f76dc987e8b89ccddb1f55df5024d52
noriyuki togashi <n-togashi@xxxx.local>
2017-04-14 18:23:46 +0900

    add column
    text,image,master_id,team_id,user_id,channel_id

--- db/migrate/20170414091753_create_messages.rb
+++ db/migrate/20170414091753_create_messages.rb
@@ -1,7 +1,12 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text :text
+      t.string :image
+      t.references :master
+      t.references :team
+      t.references :user
+      t.references :channel
       t.timestamps
     end
   end

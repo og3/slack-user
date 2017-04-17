b9b1e5890b0b3ca758d8a70dc94fe27242b087a6
noriyuki togashi <n-togashi@xxxx.local>
2017-04-17 16:06:10 +0900

    add create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,6 +13,32 @@ class MessagesController < ApplicationController
     @channels = @user.channels
     # 上で取得したchannelが持っているmessageを取得する。
     @channel_messages = @channel.messages
+    # 入力されたmessageを受け取るため、インスタンスを作成する。
+    @message = Message.new
   end
 
+  def create #ここでの変数はviewでは使うことがないので、ローカル変数にする
+    @user = User.find_by(master_id: current_master.id,team_id: params[:team_id])
+    message = Message.new(create_params) #入力された値を保存する
+    if message.save
+      respond_to do |format|
+        format.html { redirect_to team_channel_messages_path}
+        format.json {
+          render json: {
+            message: message.text,
+            name: message.user_name,
+            image_url: message.image,
+            datetime: message.created_at.strftime('%Y/%m/%d %H:%M:%S')
+          }
+        } #入力されたデータを変数に入れてJSの部分に返す
+      end
+    else
+      redirect_to team_channel_messages_path, alert: 'メッセージを入力してください'
+    end
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:text, :image).merge(master_id: current_master.id, team_id: params[:team_id], channel_id: params[:channel_id], user_id: @user.id, user_name: @user.user_name)
+  end
 end

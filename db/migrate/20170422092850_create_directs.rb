60658a5bb16956d78452a140242164227d59ebc9
noriyuki togashi <n-togashi@xxxx.local>
2017-04-23 12:13:51 +0900

    delete name column on direct

--- db/migrate/20170422092850_create_directs.rb
+++ db/migrate/20170422092850_create_directs.rb
@@ -1,7 +1,6 @@
 class CreateDirects < ActiveRecord::Migration[5.0]
   def change
     create_table :directs do |t|
-      t.string :name, null: false
       t.string :team_id, null: false
       t.timestamps
     end

f9eb8e40f3354962ea9c4dd286eb5b0b6ec468c7
noriyuki togashi <n-togashi@xxxx.local>
2017-04-24 10:49:07 +0900

    create direct_links method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # direct_messageのリンク名を作成する。
+  def direct_links
+    conbined_users = ""
+    @directs.each do |direct|
+      direct.users.each do |user|
+        conbined_users += user.user_name + ","
+      end
+    end
+    return conbined_users
+  end
 end

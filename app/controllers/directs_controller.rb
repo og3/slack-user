0d053e51b7b4f05752eb83849559a373b5de767b
noriyuki togashi <n-togashi@xxxx.local>
2017-04-25 17:01:26 +0900

    create edit function

--- app/controllers/directs_controller.rb
+++ app/controllers/directs_controller.rb
@@ -1,7 +1,7 @@
 class DirectsController < ApplicationController
   # 常にmasterのログインを要求する
   before_action :authenticate_master!
-  # before_action :set_direct, only:[:edit, :update, :destroy]
+  before_action :set_direct, only:[:edit, :update, :destroy]
 
   def new
     @direct = Direct.new
@@ -14,6 +14,7 @@ class DirectsController < ApplicationController
     @direct = Direct.new(direct_params)
     # directを作成したuserをdirectに入れる。
     @direct.users << @user
+    # binding.pry
     if @direct.save
       # redirect先にteam_idとdirect_idを持っていく
        redirect_to team_direct_messages_path(params[:team_id], @direct.id), notice: 'Success to create direct!!'
@@ -23,14 +24,47 @@ class DirectsController < ApplicationController
     end
   end
 
+  def edit
+    @users = @direct.users
+  end
+
+  def update
+    if @direct.update(direct_params)
+      # directにmasterのuserが属していない場合、team一覧にリダイレクトする。
+      if (@direct.users & current_master.users).empty?
+        redirect_to team_path(params[:team_id]), notice: 'you left channel'
+      else
+        redirect_to team_direct_messages_path(params[:team_id], @direct.id), notice: 'Success to edit channel!!'
+      end
+    else
+      flash[:alert] = 'Fail to update channel'
+      render :edit
+    end
+  end
+
+  def index
+    @user = User.where(master_id: current_master.id,team_id: params[:team_id])
+    # @userが持っている全てのdirectを取得する。
+    # 現在エラー発生！！nomethodエラー
+    @directs = @user.directs
+  end
+
+  def destroy
+    # channelを削除する。
+    @direct.destroy
+    # teamのshowに飛ばすために現在のteam_idを取得。
+    @team = Team.find(params[:team_id])
+    redirect_to team_path(@team.id), notice: 'Success to delete channel!!'
+  end
+
   private
   def direct_params
     # directを作成するときにteam_idが必要なのでmergeする。
     params.require(:direct).permit({ user_ids:[] }).merge(team_id: params[:team_id])
   end
 
-  # def set_direct
-  #   # 表示するdirectのidを持ってくる。
-  #    @direct = Direct.find(params[:id])
-  # end
+  def set_direct
+    # 表示するdirectのidを持ってくる。
+     @direct = Direct.find(params[:id])
+  end
 end
